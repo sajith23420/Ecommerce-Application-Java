dd78e53f969c7eb3c183f3438348daff09e25d23
Hansi Mudannayaka <94964835+HansiMudannayaka@users.noreply.github.com>
2024-04-25 23:59:17 +0530

    Create login.java

---
+++ login.java
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Login form</title>
+    </head>
+    <body>
+        
+        <div class="container">
+            <div class="form-box">
+                <form action="" name="Formfill" method="POST">
+                    <h2>Sign Up</h2>
+                    <p id="alert"></p>
+                    <div class="input-box">
+                        <input type="text" name="Username" placeholder="User Name">
+                    </div>
+                    <div class="input-box">
+                        <input type="email" name="Email" placeholder="Email">
+                    </div>
+                    <div class="input-box">
+                        <input type="password" name="Password" placeholder="Password">
+                    </div>
+                    <div class="input-box">
+                        <input type="password" name="cPassword" placeholder="Conform Password">
+                    </div>
+                    <div class="btn1" style="margin-top: 40px;">
+                        <input type="submit" class="btn1">
+                    </div>
+                    <br>
+                    <div>
+                        <span>I Already Have An Account <a href="signin.html">Login</a>
+                        </span>
+                    </div>
+            </div>
+            </form>
+           
+        </div>
+        
+    </body>
+</html>
